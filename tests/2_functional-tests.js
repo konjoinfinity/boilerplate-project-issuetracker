f95b22920d8ee5acf070ed69735e3500e2db47f8
Wesley Scholl <clients@konjoweb.com>
2023-02-27 11:17:31 -0500

    2 tests now passing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -31,6 +31,29 @@ suite('Functional Tests', function() {
           })
       })
 
+      suite('POST (create) request at /api/issues/apitest to create with only required fields filled', () => {
+        test('Required fields filled (Valid) create issue',(done) => {
+          chai
+            .request(server)
+            .get('/api/issues/apitest')
+            .send({
+                issue_title: 'Issue title',
+                issue_text: 'Issue text',
+                created_by: 'This author'
+            })
+            .end((err, res) => {
+              assert.equal(res.status, 200);
+              assert.equal(res.body.issue_title, 'Issue title');
+              assert.equal(res.body.issue_text, 'Issue text');
+              assert.equal(res.body.created_by, 'This author');
+              assert.equal(res.body.assigned_to, '');
+              assert.equal(res.body.open, true);
+              assert.equal(res.body.status_text, '');
+            });
+            done();
+          })
+      })
+
 });
 
 //       Create an issue with every field: POST request to /api/issues/{project}✅
