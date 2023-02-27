eb8336ed426bb7d9e9907ec924ca5ccedadfb055
Wesley Scholl <clients@konjoweb.com>
2023-02-27 08:34:27 -0500

    updated connection, db was undefined

--- dbconnection.js
+++ dbconnection.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
+let db;
+
 if (process.env.NODE_ENV == "production") {
-  const db = mongoose.connect(process.env.MONGO_URI, {
+  db = mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
@@ -14,7 +16,7 @@ if (process.env.NODE_ENV == "production") {
     } else {console.log("Production Database Connection Successful");}
     })
 } else {
-  const db = mongoose.connect("mongodb://localhost/issuetracker", {
+  db = mongoose.connect("mongodb://localhost/issuetracker", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
