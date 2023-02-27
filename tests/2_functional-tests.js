9d6fe5193d79900d56b21f368e8a24564bd3d03a
Wesley Scholl <clients@konjoweb.com>
2023-02-27 08:10:31 -0500

    project setup

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,26 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
   
+
+    // suite("GET request at /api/convert to convert with no number", () => {
+    //     test("[No number]kg Conversion to Gallons (Valid)",(done) => {
+    //       chai
+    //         .request(server)
+    //         .get("/api/convert")
+    //         .query({ input: "kg" })
+    //         .end((err, res) => {
+    //           assert.equal(res.status, 200);
+    //           assert.equal(res.body.initNum, 1);
+    //           assert.equal(res.body.initUnit, "kg");
+    //           assert.equal(res.body.returnNum, 2.20462, 0.1);
+    //           assert.equal(res.body.returnUnit, "lbs");
+    //           done();
+    //         });
+    //       })
+    //   })
+
+    
+
+
+
 });
