46d85dc24b5c101a5bb51c360b117c2b885f8230
Wesley Scholl <clients@konjoweb.com>
2023-02-28 00:20:15 -0500

    updated connection for submission

--- dbconnection.js
+++ dbconnection.js
@@ -4,20 +4,20 @@ mongoose.set('strictQuery', false);
 
 let db;
 
-if (process.env.NODE_ENV == "production") {
+// if (process.env.NODE_ENV == "production") {
   db = mongoose.connect(process.env.MONGO_URI, {
   }, function(err) {
     if (err) {
         throw err;
     } else {console.log("Production Database Connection Successful")}
     })
-} else {
-  db = mongoose.connect("mongodb://localhost/issuetracker", {
-  }, function(err) {
-    if (err) {
-        throw err;
-    } else {console.log("Development Database Connection Successful")}
-    });
-}
+// } else {
+//   db = mongoose.connect("mongodb://localhost/issuetracker", {
+//   }, function(err) {
+//     if (err) {
+//         throw err;
+//     } else {console.log("Development Database Connection Successful")}
+//     });
+// }
 
 module.exports = db;
